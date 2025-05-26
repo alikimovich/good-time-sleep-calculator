d888a3da551e0bb7f1a59c1e2f2b70bcf49682ef
Andrei Alikimovich <alikimovich@gmail.com>
2025-05-25 22:13:33 -0700

    Initial Commit

---
+++ good-time/good_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  good_timeApp.swift
+//  good-time
+//
+//  Created by Andrei Alikimovich on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct good_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ good-timeTests/good_timeTests.swift
@@ -0,0 +1,17 @@
+//
+//  good_timeTests.swift
+//  good-timeTests
+//
+//  Created by Andrei Alikimovich on 5/25/25.
+//
+
+import Testing
+@testable import good_time
+
+struct good_timeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
